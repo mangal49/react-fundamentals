1145449a50dc7fa0b845cf11661cdfbbd8886c9e
montri <montri.0337@gmail.com>
2017-05-09 16:48:20 +0700

    Add Route (End Part5)

---
+++ app/config/routes.js
@@ -0,0 +1,19 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
+var browserHistory  = ReactRouter.browserHistory;
+var IndexRoute = ReactRouter.IndexRoute;
+var Main = require('../components/Main');
+var Home = require("../components/Home");
+
+var routes = (
+    <Router history={browserHistory}>
+        <Route path='/' component={Main}>
+            <IndexRoute component={Home} />
+        </Route>
+    </Router >
+);
+
+module.exports = routes;
